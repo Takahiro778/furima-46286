dc651749a5d4106b04011afb76910ba20a9af783
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-13 14:53:49 +0900

    バリデーション実装
    Deviseが既に持つバリデーション（email必須/一意/@含む、PW必須/6文字以上、確認一致）は再定義しない。追加分だけ実装。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
+# app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
+  PW_COMPLEXITY = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
+
+  validates :nickname, :last_name, :first_name,
+            :last_name_kana, :first_name_kana, :birth_date, presence: true
+
+  validates :last_name, :first_name, format: { with: ZENKAKU_REGEX, message: 'は全角で入力してください' }
+  validates :last_name_kana, :first_name_kana, format: { with: KATAKANA_REGEX, message: 'は全角カタカナで入力してください' }
+
+  # Deviseのlengthは6以上。半角英数字混合だけ追加で担保
+  validates :password, format: { with: PW_COMPLEXITY, message: 'は半角英数字混合で入力してください' }, if: :password_required?
 end
