610e27a84f52ea54b8d2294ebb3401acaf4e2b3c
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-13 17:01:00 +0900

    ビューのname属性の整合、会員登録・ログインボタンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,15 @@ class User < ApplicationRecord
   KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
   PW_COMPLEXITY = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
 
-  validates :nickname, :last_name, :first_name,
+    validates :nickname, :last_name, :first_name,
             :last_name_kana, :first_name_kana, :birth_date, presence: true
 
-  validates :last_name, :first_name, format: { with: ZENKAKU_REGEX, message: 'は全角で入力してください' }
-  validates :last_name_kana, :first_name_kana, format: { with: KATAKANA_REGEX, message: 'は全角カタカナで入力してください' }
+  validates :last_name, :first_name, format: { with: ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name_kana, :first_name_kana, format: { with: KATAKANA_REGEX, message: 'は全角（カタカナ）で入力してください' }
 
-  # Deviseのlengthは6以上。半角英数字混合だけ追加で担保
-  validates :password, format: { with: PW_COMPLEXITY, message: 'は半角英数字混合で入力してください' }, if: :password_required?
+  # Deviseのバリデーション（presence, length）に加えて、フォーマット（英数字混合）のバリデーションを追加
+  # allow_blank: true を設定し、presenceチェックはDeviseに任せることで、エラーメッセージの重複を防ぐ
+  validates :password, format: { with: PW_COMPLEXITY, message: 'は半角英数字混合で入力してください' }, allow_blank: true, if: :password_required?
 end
+
+
