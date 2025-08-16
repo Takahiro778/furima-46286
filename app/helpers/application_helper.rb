f58961f4ec2b1698cbe1d879a42324802398ee65
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-16 17:47:15 +0900

    ApplicationHelper修正とBasic認証を本番環境のみに制限

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
-module ItemsHelper
-  def postage_label(item)
-    case item.postage_burden # 例: enum :postage_burden, { seller: 0, buyer: 1 }
-    when "seller" then "送料込み(出品者負担)"
-    when "buyer"  then "着払い(購入者負担)"
-    else               "送料未設定"
-    end
-  end
+module ApplicationHelper
 end
