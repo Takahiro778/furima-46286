b6967e4e7fe561db678885ed652ad8bb1759654f
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-21 14:48:07 +0900

    購入フォームの実装/単体テスト、buildingのNULL許容、test環境の安定化

--- db/migrate/20250820125359_change_building_null_on_shipping_addresses.rb
+++ db/migrate/20250820125359_change_building_null_on_shipping_addresses.rb
@@ -1,5 +1,5 @@
-class ChangeBuildingNullOnShippingAddresses < ActiveRecord::Migration[7.1]
+class ChangeBuildingNullOnAddresses < ActiveRecord::Migration[7.1]
   def change
-    change_column_null :shipping_addresses, :building, true
+    change_column_null :addresses, :building, true
   end
 end
