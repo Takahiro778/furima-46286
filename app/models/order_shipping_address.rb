61c1e4a92e213ebdcff7fd6de0be686c6706f24f
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-21 15:45:07 +0900

    Formオブジェクトの修正

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -32,10 +32,16 @@ class OrderShippingAddress
   validates :user_id, presence: true
   validates :item_id, presence: true
 
-  def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
-                   addresses: addresses, building: building,
-                   phone_number: phone_number, order_id: order.id)
+def save
+  order = Order.create(user_id: user_id, item_id: item_id)
+  ShippingAddress.create(
+    postal_code:   postal_code,
+    prefecture_id: prefecture_id,
+    city:          city,
+    addresses:     addresses,
+    building:      building,
+    phone_number:  phone_number,
+    order_id:      order.id
+  )
   end
 end
